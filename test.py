3d986550bb39dc60b59cf23c3643282a4e0bd311
deku0818 <84755119+deku0818@users.noreply.github.com>
2021-07-14 18:19:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-tmp = input('输入一个五位数：')
-num = str(tmp)
+#tmp = input('输入一个五位数：')
+#num = str(tmp)
 
-if num[0] == num[-1] and num[1] == num[-2]:
-          print('ok')
-else:
-          print('no')
+#if num[0] == num[-1] and num[1] == num[-2]:
+#          print('ok')
+#else:
+print('github_buddy_test success')
